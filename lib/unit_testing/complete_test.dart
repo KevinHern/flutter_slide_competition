b950fbc82131991544eb66f975c187b445592df8
Kevin Hernandez <hernandez.kevin@galileo.edu>
2022-02-14 18:22:30 -0600

    Adapted complete_test to the new bag

--- lib/unit_testing/complete_test.dart
+++ lib/unit_testing/complete_test.dart
@@ -3,12 +3,15 @@ import 'package:flutter_slide_competition/dev/data/models/bag.dart';
 import 'package:flutter_slide_competition/dev/data/models/board.dart';
 import 'package:flutter_slide_competition/dev/data/models/piece.dart';
 import 'package:flutter_slide_competition/dev/data/models/selected_piece_manager.dart';
+import 'package:flutter_slide_competition/dev/data/models/sound.dart';
 import 'package:flutter_slide_competition/dev/data/repositories/bag_management_impl.dart';
 import 'package:flutter_slide_competition/dev/data/repositories/board_management_impl.dart';
 import 'package:flutter_slide_competition/dev/data/repositories/selected_piece_management_impl.dart';
+import 'package:flutter_slide_competition/dev/data/repositories/sound_management_impl.dart';
 import 'package:flutter_slide_competition/dev/domain/repositories/bag_management_contract.dart';
 import 'package:flutter_slide_competition/dev/domain/repositories/board_management_contract.dart';
 import 'package:flutter_slide_competition/dev/domain/repositories/selected_piece_management_contract.dart';
+import 'package:flutter_slide_competition/dev/domain/repositories/sound_management_contract.dart';
 import 'package:flutter_slide_competition/dev/domain/usecases/bag_management_usecases.dart';
 import 'package:flutter_slide_competition/dev/domain/usecases/dpad_usecases.dart';
 import 'package:flutter_slide_competition/dev/domain/usecases/selected_piece_usecases.dart';
@@ -36,16 +39,18 @@ class CompleteTestScreen extends StatelessWidget {
   final Board board = Board();
   final Bag bagPieces = Bag(puzzlePieces: []);
   late final SelectedPieceManager selectedPieceManager;
+  final SoundManager soundManager = SoundManager(
+      soundPuzzleType: SoundType.NOTES,
+      template: const [MusicalNote.C, MusicalNote.E, MusicalNote.G]);
 
   // REPOSITORIOS
   late final BoardManagementRepository boardManagementRepository;
   late final BagManagementRepository bagManagementRepository;
-  late final SelectedPieceManagementRepository selectedPieceManagementRepository;
+  late final SelectedPieceManagementRepository
+      selectedPieceManagementRepository;
+  late final SoundManagementRepository soundManagementRepository;
 
-  CompleteTestScreen({
-    required this.scale,
-    Key? key
-  }) : super(key: key) {
+  CompleteTestScreen({required this.scale, Key? key}) : super(key: key) {
     board.addPiece(
       piece: Piece.withDetails(
         rotation: PieceRotation.RIGHT,
@@ -118,21 +123,20 @@ class CompleteTestScreen extends StatelessWidget {
 
     // TODO: Referenciar a ambos
     // Las piezas comienzan en board pero se mueven a bag
-    selectedPieceManager = SelectedPieceManager(puzzlePieces: board.puzzlePieces);
+    selectedPieceManager =
+        SelectedPieceManager(puzzlePieces: board.puzzlePieces);
     //selectedPieceManager = SelectedPieceManager(puzzlePieces: bagPieces.pieces);
 
     // Repositories Initialization
     selectedPieceManagementRepository = PieceManagementRepositoryImpl(
-        selectedPieceManager: selectedPieceManager
-    );
+        selectedPieceManager: selectedPieceManager);
 
-    bagManagementRepository = BagManagementRepositoryImpl(
-        bag: bagPieces
-    );
+    bagManagementRepository = BagManagementRepositoryImpl(bag: bagPieces);
 
-    boardManagementRepository = BoardManagementRepositoryImpl(
-        board: board
-    );
+    boardManagementRepository = BoardManagementRepositoryImpl(board: board);
+
+    soundManagementRepository =
+        SoundManagementRepositoryImpl(soundManager: this.soundManager);
   }
 
   @override
@@ -147,6 +151,7 @@ class CompleteTestScreen extends StatelessWidget {
           selectedPieceManagementRepository: selectedPieceManagementRepository,
           boardManagementRepository: boardManagementRepository,
           bagManagementRepository: bagManagementRepository,
+          soundManagementRepository: soundManagementRepository,
         ),
       ),
     );
@@ -159,38 +164,32 @@ class AllProviders extends StatelessWidget {
   final SelectedPieceManager selectedPieceManager;
   final Widget child;
 
-  const AllProviders({
-    required this.board,
-    required this.bagPieces,
-    required this.selectedPieceManager,
-    required this.child,
-    Key? key
-  }) : super (key: key);
+  const AllProviders(
+      {required this.board,
+      required this.bagPieces,
+      required this.selectedPieceManager,
+      required this.child,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<BoardUI>(
-            create: (context) => BoardUI(board: board)
-        ),
-        ChangeNotifierProvider<BagUI>(
-          create: (context) => BagUI(bag: this.bagPieces),
-        ),
-
-        ChangeNotifierProvider<BoardPieceManagerUI>(
-          create: (context) => BoardPieceManagerUI(),
-        ),
-        ChangeNotifierProvider<SelectedPieceManagerUI>(
-          create: (context) => SelectedPieceManagerUI(),
-        ),
-
-        ChangeNotifierProvider<ToggleRotation>(
-          create: (context) => ToggleRotation(canRotate: true),
-        ),
-      ],
-      child: child
-    );
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<BoardUI>(
+          create: (context) => BoardUI(board: board)),
+      ChangeNotifierProvider<BagUI>(
+        create: (context) => BagUI(bag: this.bagPieces),
+      ),
+      ChangeNotifierProvider<BoardPieceManagerUI>(
+        create: (context) => BoardPieceManagerUI(),
+      ),
+      ChangeNotifierProvider<SelectedPieceManagerUI>(
+        create: (context) => SelectedPieceManagerUI(),
+      ),
+      ChangeNotifierProvider<ToggleRotation>(
+        create: (context) => ToggleRotation(canRotate: true),
+      ),
+    ], child: child);
   }
 }
 
@@ -199,6 +198,7 @@ class CompleteTestBody extends StatelessWidget {
   final SelectedPieceManagementRepository selectedPieceManagementRepository;
   final BoardManagementRepository boardManagementRepository;
   final BagManagementRepository bagManagementRepository;
+  final SoundManagementRepository soundManagementRepository;
 
   static const rotationCycle = [
     PieceRotation.UP,
@@ -207,55 +207,61 @@ class CompleteTestBody extends StatelessWidget {
     PieceRotation.LEFT
   ];
 
-  const CompleteTestBody({
-    required this.scale,
-    required this.selectedPieceManagementRepository,
-    required this.boardManagementRepository,
-    required this.bagManagementRepository,
-    Key? key
-  }) : super(key: key);
+  const CompleteTestBody(
+      {required this.scale,
+      required this.selectedPieceManagementRepository,
+      required this.boardManagementRepository,
+      required this.bagManagementRepository,
+      required this.soundManagementRepository,
+      Key? key})
+      : super(key: key);
 
-  void _rotate({required BuildContext context, required RotationOrientation orientation}) {
+  void _rotate(
+      {required BuildContext context,
+      required RotationOrientation orientation}) {
     int rotation = (orientation == RotationOrientation.CLOCKWISE) ? 1 : -1;
 
     final Piece currentPiece = SelectedPieceManagementUseCases(
-        selectedPieceManagementRepository: selectedPieceManagementRepository
-    ).getCurrentSelectedPiece();
+            selectedPieceManagementRepository:
+                selectedPieceManagementRepository)
+        .getCurrentSelectedPiece();
 
-    final int currentRotationCycleIndex = rotationCycle.indexOf(currentPiece.rotation);
-    final int nextRotationCycleIndex = (currentRotationCycleIndex + rotation) % rotationCycle.length;
+    final int currentRotationCycleIndex =
+        rotationCycle.indexOf(currentPiece.rotation);
+    final int nextRotationCycleIndex =
+        (currentRotationCycleIndex + rotation) % rotationCycle.length;
 
-    BagManagementUseCases(
-        bagManagementRepository: bagManagementRepository
-    ).rotatePieceInBag(
+    BagManagementUseCases(bagManagementRepository: bagManagementRepository)
+        .rotatePieceInBag(
       puzzlePiece: currentPiece,
       newRotation: rotationCycle[nextRotationCycleIndex],
     );
     Provider.of<BagUI>(context, listen: false).update();
   }
 
-  void _move({required BuildContext context, required BoardDirection direction}) {
+  void _move(
+      {required BuildContext context, required BoardDirection direction}) {
     final Piece piece = SelectedPieceManagementUseCases(
-      selectedPieceManagementRepository: selectedPieceManagementRepository
-    ).getCurrentSelectedPiece();
+            selectedPieceManagementRepository:
+                selectedPieceManagementRepository)
+        .getCurrentSelectedPiece();
 
-    final Piece outPiece = DpadUseCases(
-      boardManagementRepository: boardManagementRepository
-    ).movePiece(direction: direction, puzzlePiece: piece);
+    final Piece outPiece =
+        DpadUseCases(boardManagementRepository: boardManagementRepository)
+            .movePiece(direction: direction, puzzlePiece: piece);
 
     Provider.of<BoardUI>(context, listen: false).update();
 
     if (!outPiece.isNullPiece) {
-      BagManagementUseCases(
-        bagManagementRepository: bagManagementRepository
-      ).addToBag(puzzlePiece: outPiece);
+      BagManagementUseCases(bagManagementRepository: bagManagementRepository)
+          .addToBag(puzzlePiece: outPiece);
 
       Provider.of<BagUI>(context, listen: false).update();
     }
   }
 
   @override
-  Widget build(BuildContext context ) {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: MyUtils.setScreenPadding(context: context),
@@ -274,39 +280,44 @@ class CompleteTestBody extends StatelessWidget {
                 DPad(
                   scale: scale,
                   isActive: true,
-                  upPress: () => _move(context: context, direction: BoardDirection.UP),
-                  rightPress: () => _move(context: context, direction: BoardDirection.RIGHT),
-                  downPress: () => _move(context: context, direction: BoardDirection.DOWN),
-                  leftPress: () => _move(context: context, direction: BoardDirection.LEFT),
+                  upPress: () =>
+                      _move(context: context, direction: BoardDirection.UP),
+                  rightPress: () =>
+                      _move(context: context, direction: BoardDirection.RIGHT),
+                  downPress: () =>
+                      _move(context: context, direction: BoardDirection.DOWN),
+                  leftPress: () =>
+                      _move(context: context, direction: BoardDirection.LEFT),
                 ),
               ],
             ),
-
-            const SizedBox(
-                height: 50,
-                width: 50
-            ),
-
+            const SizedBox(height: 50, width: 50),
             Expanded(
               child: Column(
                 children: [
                   BagWidget(
                     bagOfPieces: Provider.of<BagUI>(context, listen: true).bag,
-                    toggleRotation: Provider.of<ToggleRotation>(context, listen: true),
+                    toggleRotation:
+                        Provider.of<ToggleRotation>(context, listen: true),
                     height: 500,
-                    selectedPieceManagementRepository: selectedPieceManagementRepository,
+                    selectedPieceManagementRepository:
+                        selectedPieceManagementRepository,
+                    soundManagementRepository: this.soundManagementRepository,
+                    bagType: BagType.SPATIAL,
                   ),
                   const SizedBox(
                     height: 50,
                   ),
                   RotationButtons(
                     scale: scale,
-                    isActive:
-                    Provider.of<ToggleRotation>(context, listen: true).canRotate,
+                    isActive: Provider.of<ToggleRotation>(context, listen: true)
+                        .canRotate,
                     rotateLeft: () => _rotate(
-                        context: context, orientation: RotationOrientation.ANTICLOCKWISE),
+                        context: context,
+                        orientation: RotationOrientation.ANTICLOCKWISE),
                     rotateRight: () => _rotate(
-                        context: context, orientation: RotationOrientation.CLOCKWISE),
+                        context: context,
+                        orientation: RotationOrientation.CLOCKWISE),
                   ),
                   const SizedBox(
                     height: 50,
@@ -325,25 +336,18 @@ class CompleteTestBody extends StatelessWidget {
                 ],
               ),
             ),
-
-            const SizedBox(
-              height: 50,
-              width: 50
-            ),
-
+            const SizedBox(height: 50, width: 50),
             Column(
               children: [
                 Container(
                   color: Colors.blue,
                   width: 200,
                   height: 200,
-                  child: const Text(
-                    "Cuando crezca seré un puzzle :)",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    )
-                  ),
+                  child: const Text("Cuando crezca seré un puzzle :)",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      )),
                 ),
               ],
             ),
@@ -353,4 +357,3 @@ class CompleteTestBody extends StatelessWidget {
     );
   }
 }
-
